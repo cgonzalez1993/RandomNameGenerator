1944806ff0b5bca46fe597b44353f619b15afaae
Carlos Gonzalez <carlosg2011@gmail.com>
2021-05-30 00:28:54 -0700

    fixed LS

--- js/script.js
+++ js/script.js
@@ -2,99 +2,96 @@ let wickedCss = ["floater", "barrelRoll", "rollerRight", "rollerLeft", "hearbeat
     "rotation", "sideToSide", "zoomer", "zoomerOut", "spinner", "wiggle", "shake", "pound", "slideUp",
     "slideDown", "slideRight", "slideLeft", "fadeIn", "fadeOut", "rotateInRight", "rotateinLeft",
     "rotateIn", "bounceIn"];
+    
+let inputNames = document.getElementById("inputNames");
+let outputNames = document.getElementById("outputNames");
+let newNames = [];
+let selectedName = "";
+    
+let data = JSON.parse(localStorage.getItem("List"));
 
 
-//let studentNames = ["Adrian Segura", "Arthur Olmos", "Alex Ato", "Ann Chansey", "Bryan Stewart", "Carlos Gonzalez", "Christy Eang", "Demitrius Ovro", "Dylan McFarlin", "Hugo Mejia", "John Shamoon", "Jorge Lopez", "Joseph Racca", "Juan Morales", "Ken Martinez", "Leopoldo Manzo", "Matthew Mendoza", "Matthew Tyler Rose", "Sean Chandler", "Trevor Womack", "Victor Maldonado"];
-
-
-//Script to have the button pull a name from the studentNames list
 let getName = document.getElementById("getName").addEventListener("click", function () {
-    randomNameGenerator();
-    //     let cssRandom = Math.floor(Math.random() * wickedCss.length);
-    // displayName.className = "";
-    displayName.className = "card-text white center"
+    if (newNames.length <= 0) {
+        alert("There are no names listed. Please enter some names.")
+    }
+    else {
+        randomNameGenerator();
+        let cssRandom = Math.floor(Math.random() * wickedCss.length);
+        displayName.className = "list-group-item bleu center cremeColour radius" + wickedCss[cssRandom];
+    }
 });
 
-let selectedName = "";
-
 function randomNameGenerator() {
+    // let selectedName = "";
     selectedName = newNames[Math.floor(Math.random() * newNames.length)];
     displayName.innerText = selectedName; //Script for button to display a random name wherever you set the id
 }
 
 
-let inputNames = document.getElementById("inputNames");
-let outputNames = document.getElementById("outputNames");
-let newNames = [];
-
-let data = JSON.parse(localStorage.getItem("List"));
-
-
 inputNames.addEventListener("keypress", function (e) {
-    //console.log(e);
     if (e.code == "Enter") {
         appendElements(inputNames.value);
-    }
-
+    } 
 });
 
-function appendElements(checkList) {
-    console.log(checkList);
-
+function appendElements(addedName) {
+    console.log(addedName);
     if (inputNames.value == "" || inputNames.value == null) {
         alert("Enter something other than nothing...");
     }
-    else if (inputNames.value == "Something" || inputNames.value == "something") {
+    else if (inputNames.value.toLowerCase() == "something") {
         alert("Ohhh you think you're funny, huh? Try again...");
     }
-    else if (inputNames.value == "Nothing" || inputNames.value == "nothing") {
+    else if (inputNames.value.toLowerCase() == "nothing") {
         alert("Smart ass. Try again...");
+    }
+    else if (newNames.includes(inputNames.value) == true) {
+        alert("This name has already been entered. Please enter a new name!")
     }
     else {
         let pElement = document.createElement("p");
-        pElement.innerText = checkList;
+        pElement.innerText = addedName;
+        pElement.className = "list-group-item center bleu radius px-5";
+        outputNames.appendChild(pElement);
+        inputNames.value = "";
+        newNames.push(addedName);
+        localStorage.setItem("List", JSON.stringify(newNames));
 
         pElement.addEventListener("click", function (e) {
-            //debugger              
-            newNames.forEach(x => {
-                if (e.target.innerText != x) {
-                    console.log(x);
-                    newNames = [];
-                    newNames.push(x)
+            for (i = 0; i < newNames.length; i++) {
+                if (e.target.innerText == newNames[i]) {
+                    let nameToRemove = newNames.indexOf(e.target.textContent);
+                    newNames.splice(nameToRemove, 1);
                     localStorage.setItem("List", JSON.stringify(newNames))
                     e.target.remove();
                 }
-
-            })
-
-
+            }
         });
-
-        outputNames.appendChild(pElement);
-        inputNames.value = "";
-
-        newNames.push(checkList);
-
-        localStorage.setItem("List", JSON.stringify(newNames))
     }
+    console.log(newNames);
 }
-
 
 if (data != "" || data != null) {
     for (let i = 0; i < data.length; i++) {
         let pElement = document.createElement("p");
-
         pElement.innerText = data[i];
-
-        pElement.addEventListener("click", function (e) {
-            e.target.remove();
-        });
-
+        pElement.className = "list-group-item center bleu radius px-5";
         outputNames.appendChild(pElement);
         newNames.push(data[i]);
-        localStorage.setItem("List", JSON.stringify(newNames))
+        localStorage.setItem("List", JSON.stringify(newNames));
+
+        pElement.addEventListener("click", function (e) {
+            for (i = 0; i < newNames.length; i++) {
+                if (e.target.innerText == newNames[i]) {
+                    let nameToRemove = newNames.indexOf(e.target.textContent);
+                    newNames.splice(nameToRemove, 1);
+                    localStorage.setItem("List", JSON.stringify(newNames));
+                    e.target.remove();
+                }
+            }
+        });
     }
 }
-
 
 
